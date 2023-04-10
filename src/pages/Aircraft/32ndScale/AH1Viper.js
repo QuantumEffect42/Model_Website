f5e9554bb31cd6483d07f755d9aab0daef4207c0
nmorri <nmorri47@uwo.ca>
2023-04-09 21:47:40 -0400

    Content for AH-1 page

--- src/pages/Aircraft/32ndScale/AH1Viper.js
+++ src/pages/Aircraft/32ndScale/AH1Viper.js
@@ -1,10 +1,184 @@
 import React from "react";
 import Header from "../../../components/Navbar/Header";
+import "../Post.css"
+import image1 from "../../../images/Aircraft/AH1Viper/20221125_000556.jpg"
+import image2 from "../../../images/Aircraft/AH1Viper/20220326_012849_HDR.jpg"
+import image3 from "../../../images/Aircraft/AH1Viper/20220401_004842.jpg"
+import image4 from "../../../images/Aircraft/AH1Viper/20221017_015044.jpg"
 
 const AH1ZViper = () =>{
     return(
         <>
             <Header />
+            <div className="Content">
+                <div className="Titlebar">
+                    <h1 className="Kit-Name">Academy 1:32 AH-1Z Viper</h1>
+                    <h2 className="Kit-Desc">An absolutely stunning display of engineering and precision.</h2>
+                </div>
+
+                <div className="Post">
+                    <img src={image1} alt="First" className="Post-Embed-Image"></img>
+
+                    <p className="Post-Text">
+                        So I was finally able to get my hands on the new Viper kit from Academy. Ever since I heard some of the reviews 
+                        for this kit, I knew I just had to add one to my shelf. Opening the kit and examining the sprues, I could immediately 
+                        tell that this kit was absolutely worth hype. The molding on the parts was out of this world; the entire tail beam
+                        from the main body to the sabilizers and the tail rotor, was cast using slide molding. Words can't really express how
+                        happy seeing how well engineered this kit was, I was over the moon that I wouldn't have to sacrificae much, if any,
+                        detail from surface of this kit filling gaps and pannel lines.
+                    </p>
+                    <p className="Post-Text">
+                        Unfortunately, I don't have as many pictures of the build process itself, but I'll try my best to recount everything
+                        as we walk through the entire project from start to finish.
+                    </p>
+
+                    <h2 className="Section">The Build</h2>
+                    <p className="Post-Text">
+                        As mentioned above, the build quality on this kit was fantastic. Most parts fit together well enough that you could
+                        realistically hold large portions of the kit together without any glue. Despite this though, all the parts were glued
+                        together with good old trusty Tamiya Extra Thin Cement.
+                    </p>
+                    <p className="Post-Text">
+                        As with most aircraft, the first thing we start with is the cockpit. I haven't done any 32nd scale cockpits since I usually
+                        only build 48th scale aircraft, so the amount of detail I was able to go into was quite astonishing to me. I actually ended
+                        up leaving most of the cockpit parts unglued and separate from each other to allow for easier airbrushing. I used Tamiya
+                        Xf-1 to paint all cockpit parts in a flat black layer as per the instructions and the real life AH-1Z cockpit and then I moved
+                        on to painting nearly every individual swtich and button Xf-2 flat white to give everything some definintion. Let me tell you,
+                        this process with my smallest brush probably took an entire evening. Behind the pilot's seat in the back of the aircraft, there is
+                        a switchboard with seemingly endless buttons. Once this was done, any and all mistakes were carefully corrected with some more
+                        Xf-1. There are a few switches in the AH-1Z that have red covers, thus, these were painted with some Xf-7 flat red. Next, there
+                        were some small decals the instructions called for that would be the screens of the MFDs in both the pilot's and CPG's seats.
+                        They were were a blank green to indicate they were not turned on, which would work perfect since I planned to have this helicopter
+                        be parked on the ground with the canopy's open. Once those were applied I tried somethng new that I had seen online at the time;
+                        I covered the decals in a layer of X-22 gloss clear to give them the sheen a proper screen would have. The issue I had with this method
+                        was that it never really dried in a consistent clear layer like I had hoped it would. I believe this was due to the fact that the
+                        varnish was thinned since it was the stuff I usually use with my airbrush, I would like to try it once more in the future with a
+                        thicker varnish to see if I can get any different results. Once these decals were in, I did a bit of light weathering by just
+                        doing some dry brushing with some X-11 chrome silver to give the edges a bit of wear and tear, and I also added a bit of dust
+                        with some earth pigments near the rudder pedals and on the edges where the pilot and CPG would climb in. Once I was happy with the
+                        cockpit, I sealed it up in the 2 part fuselage.
+                    </p>
+
+                    <img src={image2} alt="Second" className="Post-Embed-Image"></img>
+
+                    <p className="Post-Text">
+                        The rest of the kit was assembled just following the instructions with the occasional piece of photoetch being added here and there.
+                        I'm sure you're probably sick of hearing me talk about how nice this kit was to build, but I'm going to say it once more, just in case
+                        it hasn't quite gotten through yet. All the parts of this kit fit so well together. I can't really recall a kit that I've had to do less
+                        sanding and filling on. There were just 2 sections that I had to fill: A section just behind the cockpit and in fron of the main rotor, and
+                        the top of the part that holds the ball camera. Both of those sections were filled with a small amount of Tamiya putty, and then sanded down
+                        accordingly. There was hardly an detail lost so very little work had to go into restoring anything lost. Again, a lot of parts were left
+                        off the model to make my life easier when airbrushing, especially with a model as large as this in my tiny spray booth. 
+                    </p>
+
+                    <img src={image3} alt="Third" className="Post-Embed-Image"></img>
+
+                    <p className="Post-Text">
+                        One of the places I messed up on the build of this kit was on the ball camera at the very front; I may have accidentally glued it so that
+                        it can't rotate from side to side. I followed the instructions so that it can still move up and down, but I did glue it so that it can't
+                        rotate from side to side. Before I realized the screw up, the extra thin cement has long since set. Additionally, the part at the very
+                        end of the tail that I believe is used as a brace to stop the tail from hitting the ground in a hard landing, broke off and was devoured
+                        by the carpet monster. I was shocked at the appetite of the carpet monster this time since parts that big don't usually go missing, but alas,
+                        the search for it proved fruitless. Despite this setback though, I was able to fashion and make a replacement out of some varrying thickness
+                        PLA sheets I had lying around. I used the instructions to give me some approximate dimmensions for the piece and was able to make something
+                        I think is a suitable replacement; just promise me you won't look too closely at it.
+                    </p>
+
+                    <h2 className="Section">Painting</h2>
+
+                    <p className="Post-Text">
+                        Now that everything was all assembled, we can move to painting everything. I realized very quickly that I might need to upgrade my tiny
+                        spray booth if I'm going to continue doing models of this size; maneuvering the kit in that tiny space was sketchy at times. Regardless though,
+                        I had some Tamiya spray can primer that I needed to use up so the entire model was primed using that after masking off the canopy sections
+                        very thoroughly. Many times, I don't bother using a primer, but since this model had photoetch parts on it, I knew that I had to take the extra
+                        time to level the surface for this kit. 
+                    </p>
+                    <p className="Post-Text">
+                        Once everything was primed and ready for painting, I wanted to try another new technique I hadn't experimented with before; Pre-shading.
+                        Since I'm primarily an armour modeller, I haven't done an aircraft since I upgraded my game and got an airbrush and I figured that this would
+                        be the perfect time to give it a shot. I used a 0.2mm needle to carefully airbrush over every rivet and panel line giving it a highlight against
+                        the light grey primer. 
+                    </p>
+
+                    <img src={image4} alt="Fourth" className="Post-Embed-Image"></img>
+
+                    <p className="Post-Text">
+                        Once the highlights were all done, there was another conundrum I had to figure out how to deal with; I didn't have a light or dark ghost grey
+                        paint that I could use. So, I had 2 options that I could choose from: I could by some of the paint I needed online and potentially end up
+                        waiting weeks for it to come in, or I could try mixing my own. Since I didn't want to delay my project for potentially weeks, I opted for the
+                        latter, mixing my own colours our of the various Tamiya paints I had in my drawer. I ended up getting relatively close to the called for
+                        colour scheme using light grey, white, and some vary small amounts of black and blue as well. Once I had my colours all mixed and thinned properly,
+                        I started carefully applying thin layers over the post shadding, being very careful to not remove the effect entirely under the grey base coat.
+                        Once all the paint was applied, I was very happy with the effect the pre-shading had on the kit. The effect was subtle enough that it gave the paint
+                        job a nice depth, but not being too eye-popping that it would look out of place in the actual helicopter. I masked off the tail section and painted
+                        the some of the parts in a gloss black and sealed the entire paint job in a couple layers of X-22 gloss clear, to prepare for decalling.
+                    </p>
+
+                    <h2 className="Section">Decals</h2>
+
+                    <p className="Post-Text">
+                        I'm going to preface this section with saying that I think decalling is one of the more tedious and boring parts of the building any kit. But let
+                        me tell you, if you love decals and the idea of spending several evenings applying them makes you hit the roof from excitement, then my goodness is
+                        this kit for you. In all seriousness though, there are a ton of decals for this kit. Being the scale that it is, I would certainly expect it to go
+                        into more detail than some of the 48th scale aircraft I built way back when I was just starting out, but that doesn't make the process of applying
+                        probably over a hundred decals any more enjoyable. I added a bit of Micro Set before placing the decal down on the surface, pressed each decal down
+                        into the surface after moving it into the right spot to remove any air that might have got trapped underneath, and to flatten out any wrinkles that
+                        appear from the decal sitting ontp of things like rivets. Once the decal was set for a bit, I applied a coat of Micro Sol to help reduce the silvering
+                        and the make the decal softer to conform better to any details they may be sitting on top of.
+                    </p>
+
+                    <p className="Post-Text">
+                        All of the decals on the main body went on with little trouble, but then came the munitions. Each Hellfire missile had 8 decals that were required to
+                        go on meaning that there were 32 decals per Hellfire rack for a grand total of 64 decals just on the radar guided missiles. The AIM-9L missiles also had around
+                        10 decals each making for the most tedious part of this build by far. Some of the decals are very awkward in their shape, espcially some of the ones
+                        for the warnings on the AIM-9Ls that made then quite a pain to get positioned correctly, and an even further pain to get them set properly. Decalling the
+                        munitions probably took me 3 evenings on their own, along with a large helping of back pain to boot. Once this was done though, I could finally seal the
+                        model with a final gloss clear coat and move on to weathering the kit, one of my favourite parts of any build.
+                    </p>
+
+                    <h2 className="Section">Weathering</h2>
+
+                    <p className="Post-Text">
+                        With this kit, I wanted to keep the weathering more subtle since a lot of the images I found online of real Vipers show then as being definitely weathered
+                        with some grime but overall they are generally on the clean side. For the main weather I used a Tamiya Panel Line Accent Colour Black as a wash to go over
+                        all the rivets and panel lines to make all the small details pop. Since there's usually so much overflow from how thin this paint is, I used some enamel thinner
+                        soaked q-tips to clean up the excess. While I was cleaning the surfaces up I created some streaking effects at the same time by pulling the wash down across
+                        the pannels in the same way water would run off them. this meant top to bottom on the majority of the fuselage and front to back on the wings and stabilizers.
+                        I'm quite happy with the effect this ended up achieving as it achieved the grime level I was going for.
+                    </p>
+
+                    <p className="Post-Text">
+                        There was however, one big issue I ended up encountering with the this wash and some of the decals. Some of the decals didn't set as well as I had thought they
+                        did and thus, they weren't sealed entirely by the gloss clear coat. This meant that when I used the wash to go over some of the rivets they were sitting on top
+                        of, the wash flowed under the decals and created a very pronounced area where the wash had pooled in certain areas. This was a bit disappointing since there
+                        really wasn't anything I could do to fix it without completely removing, and likely destroying the decals. Next time, I need to really make sure that the decals
+                        are all completely set so tehy get sealed properly by the gloss clear coat.
+                    </p>
+
+                    <p className="Post-Text">
+                        The last little bit of weathering I did on this model was adding a small oil paint dot filter to some of the pannels. I hoped to make some of the pannels stand out
+                        from each other and make the overall surface a bit less uniform. I could have definitely gone futher in this regard since I think the effect is still a bit muted
+                        but up close it does add a slight variation to the base colour schemes. Finally, I used some black oil paint to create the effect of some of the exhaust gases
+                        from the twin engines on the tail beam. I thought this kind of effect would go well to really sell the idea that this helicopter had been in service for a
+                        considerable amount of time, even if it would have normally been cleaned by the maintenance crew. After that I added one final layer of semi-gloss clear to seal
+                        all the weathering work
+                    </p>
+                    
+                    <h2 className="Section">Final Touches</h2>
+
+                    <p className="Post-Text">
+                        There were a few final things to do now that all the weathering was done. First, I attached all the armaments to the kit which included the 2 AIM-9Ls, 8 AGM-114
+                        Hellfire missiles, and 2 unguided rocket pods; Next, I painted and added all the clear parts including formation and landing lights for the kit as well as the
+                        clear sensors for each of the Hellfires. After that was the big reveal, removing the masking from the canopy and cockpit. Thankfully, the time I spent masking
+                        everything largely paid off, there was just a little bit of sepage on the very edges of the clear parts, but the entirety of the cockpit was protected from all
+                        the airbrushing. After spending a bit of time cleaning the clear parts of the canopy, the the model was completed and ready for pictures.
+                    </p>
+                </div>
+
+                <br />
+                <br />
+                <br />
+            </div>
         </>
     );
 }

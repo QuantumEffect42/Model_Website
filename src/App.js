e5c9d28e9ff49ecd46f2470e9ea65ae65734a4f4
nmorri <nmorri47@uwo.ca>
2023-04-16 13:17:34 -0400

    Changed component back to Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Home from './pages/Home';
-import AH1ZViper from './pages/Aircraft/32ndScale/AH1Viper';
 
 function App() {
   return (
     <div className="App">
-      <AH1ZViper />
+      <Home />
     </div>
   );
 }

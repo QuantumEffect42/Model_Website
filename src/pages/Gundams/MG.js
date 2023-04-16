d4f00c4ba04f0641ec5e541f99388eff30547ead
nmorri <nmorri47@uwo.ca>
2023-04-16 14:21:08 -0400

    Added RFAStray Card

--- src/pages/Gundams/MG.js
+++ src/pages/Gundams/MG.js
@@ -3,6 +3,7 @@ import Header from "../../components/Navbar/Header";
 import Card from "../../components/Cards/Card";
 import CardGrid from "../../components/Cards/CardGrid";
 import DynamesImage from "../../images/Gundams/Dynames/Dynames_1.jpg"
+import RFAstrayImage from "../../images/Gundams/RedFrameAstray/Astray_1.jpg"
 
 const MGGundams = () =>{
     return(
@@ -13,10 +14,13 @@ const MGGundams = () =>{
                     <a href="/Dynames" className="Page-Link">
                         <img src={DynamesImage} className="Thumbnail" alt="Dynames-Thumbnail"></img>
                     </a>
-                    <h2>GN-002 Dynames</h2>
+                    <h2 className="Title">GN-002 Dynames</h2>
                 </Card>
                 <Card>
-                    <h2>Test 2</h2>
+                    <a href="/" className="Page-Link">
+                        <img src={RFAstrayImage} className="Thumbnail" alt="RFAstray-Thumbnail"></img>
+                    </a>
+                    <h2>Red-Frame Astray</h2>
                 </Card>
             </CardGrid>
         </>

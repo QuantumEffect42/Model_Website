298c82bed51fca2cd9dbd92392ebe530485d36a4
nmorri <nmorri47@uwo.ca>
2023-04-16 13:15:31 -0400

    Content for the Dynames page

--- src/pages/Gundams/MasterGrades/Dynames.js
+++ src/pages/Gundams/MasterGrades/Dynames.js
@@ -1,10 +1,136 @@
 import React from "react";
 import Header from "../../../components/Navbar/Header";
+import image1 from "../../../images/Gundams/Dynames/Dynames_1.jpg"
+import image2 from "../../../images/Gundams/Dynames/20230212_144555.jpg"
+import image3 from "../../../images/Gundams/Dynames/20230212_144610.jpg"
+import image4 from "../../../images/Gundams/Dynames/20230212_144657.jpg"
+import image5 from "../../../images/Gundams/Dynames/20230212_150605.jpg"
+import image6 from "../../../images/Gundams/Dynames/20230212_150613.jpg"
+import image7 from "../../../images/Gundams/Dynames/20230212_215016.jpg"
+import image8 from "../../../images/Gundams/Dynames/20230212_215923.jpg"
+import image9 from "../../../images/Gundams/Dynames/20230212_220444.jpg"
+import image10 from "../../../images/Gundams/Dynames/20230212_222542.jpg"
+import image11 from "../../../images/Gundams/Dynames/20230212_224828.jpg"
+import image12 from "../../../images/Gundams/Dynames/20230212_224849.jpg"
+import image13 from "../../../images/Gundams/Dynames/20230213_235050.jpg"
+import image14 from "../../../images/Gundams/Dynames/20230213_235055.jpg"
+import image15 from "../../../images/Gundams/Dynames/20230213_235101.jpg"
+import image16 from "../../../images/Gundams/Dynames/20230214_002702.jpg"
+import image17 from "../../../images/Gundams/Dynames/20230214_002707.jpg"
+import "../../Post.css"
 
 const Dynames = () =>{
     return(
         <>
             <Header />
+            <div className="Content">
+                <div className="Titlear">
+                    <h1 className="Kit-Name">GN-002 Dynames</h1>
+                    <h2 className="Kit-Desc">Lockon... Lockon... Lockon...</h2>
+                </div>
+
+                <div className="Post">
+                    <img src={image1} alt="First" className="Post-Embed-Image"></img>
+
+                    <p className="Post-Text">
+                        Finally, I got around to getting my first MG kit from the 00 series, one of my favourites from the Gundam shows I've watched thus far.
+                        This also happens to be my favourite Mobile Suit from any of the series I've watched, the sniper role with the gun camera that is hidden
+                        behind the V-fin is so cool to me, not to mention the stud of a pilot that Lockon Stratos is. Coincidentally, I've also heard that this
+                        is the Master Grade of all Master Grades; meaning this kit is far and away the best Master Grade kit Bandai has to offer. As such, I
+                        decided that I had to throw everything and the kitchen sink at this kit to make it far and away the best kit I've ever done. This kit
+                        is going to have LED lights not just in the eyes, but in the GN-Drive, and behind all the clear parts in both the legs and the arms with
+                        the option to flip a switch to enable Trans-Am mode to change most of the LEDs to red using Arduinos and lots and lots of micro-leds.
+                    </p>
+
+                    <h2 className="Section">The Build</h2>
+                    <p className="Post-Text">
+                        Like all other Gundam kits from Bandai, there's no glue required to assemble anything in the base kit, which is a nice change of pace from some
+                        of the other kits I usually build. The engineering on how the sections are build is incredibly impressive, definitely earning the kit it's
+                        reputation as one of the best Bandai has ever made. It's certainly the best Master Grade I've built thus far, that's for sure. To give an example,
+                        the moveable V-fin that sits on Dyanmes' head hides the Gun Camera it uses for sniping when not in use, but the 2 halves of the head sandwitch a
+                        part that slides back and forth to allow the V-fin to rotate down, covering the eyes and revealing the Gun Camera. Clever engineering like this is
+                        everywhere on this kit and it's very evident that a lot of love and care went into the design of the parts.
+                    </p>
+                    <p className="Post-Text">
+                        Because of all this care taken in the design, the built has some of the best posability of any Gundam in my collection thus far. The amount of
+                        movement it has even in the torso alone puts many other kits to shame. This of course, let me have hours of fun meticulously trying to get the
+                        kit in the right poses for some cool photos before I moved onto the next step of the build.
+                    </p>
+
+                    <img src={image2} alt="Second" className="Post-Embed-Image"></img>
+                    <img src={image3} alt="Third" className="Post-Embed-Image"></img>
+                    <img src={image4} alt="Fourth" className="Post-Embed-Image"></img>
+                    <img src={image5} alt="Fifth" className="Post-Embed-Image"></img>
+                    <img src={image6} alt="Sixth" className="Post-Embed-Image"></img>
+
+                    <h2 className="Section">The Build - Part 2</h2>
+                    <p className="Post-Text">
+                        With this kit being one of my favourites ever, I wanted to really up the detail to a new level. I'm not quite at the level where I'm super confident
+                        in my ability to create a ton of extra details from PLA sheets, putty, and scribe extra panel lines in a way that actually looks good, so I chose
+                        a different option to take this kit to the next level: Fortune Meow Studio's Resin Conversion Kit. This additional part set adds a ton of extra and
+                        replacement parts for the base kit that are cast out of resin to have a ton more detail. 
+                    </p>
+                    <p className="Post-Text">
+                        <b>A WORD OF WARNING</b>: Resin dust is very bad for your health and it should not be inhaled at all. All the parts were assembled and prepped in my
+                        spray booth to pull dust away from my face. Additionally, a ton of ventilation was used to keep fresh air circulating in my room and a full
+                        respirator was worn at all time during sanding and assembly. Afterwards, you should wipe down any surfaces that might contain resin dust with a wet
+                        paper towel to make sure the majority of dust is removed from your work area.
+                    </p>
+                    <p className="Post-Text">
+                        Once I got this kit in the mail, it was just a matter of working through the instructions and finding which parts needed to be replaced from the
+                        original kit. This was a bit harder and more time consuming than I had originally imagined it would be since I couldn't actually read the language
+                        the instructions were printed in, so all I had to go off was the pictures they offered. It went quite well overall but it did take me some time to
+                        wrack my brain and figure out how they moved from step to step in some sections. I did give myself a bit of a scare too when assembling one of the
+                        arms from the conversion kit. The replacement elbow joint sits inside another armour piece to make it fit better with the other arm parts. Well, I,
+                        in my infinite wisdom, decided that using some super thin CA glue to hold these parts together was the absolute best idea. What I didn't consider at
+                        the time was, "Well, what if this thin glue seeps into the elbow joint itself?", and you'd never guess that that's exactly what happened. I had to
+                        frantically pull the parts apart so the arm wouldn't permenantly be stuck in a super awkward position and in, doing that, I ended up gluing a part of
+                        my glove to some one of the resin parts that needed to be sanded out.
+                    </p>
+                    <p className="Post-Text">
+                        Despite this escapade though, the rest of the kit went together really well all things considered. I didn't glue any other resin parts together besides
+                        the elbow joints into their housing so that the kit would actually come apart so that I can airbrush it, so in some of the pictures, they're all just
+                        precariously hanging onto the kit by whatever little bit of fricition they have between each other. You can see some of the before and after pictures
+                        for some of the parts below to get an idea of the level of deatil I'm talking about.
+                    </p>
+                    
+                    <img src={image7} alt="Seventh" className="Post-Embed-Image"></img>
+                    <img src={image8} alt="Eigth" className="Post-Embed-Image"></img>
+                    <img src={image9} alt="Ninth" className="Post-Embed-Image"></img>
+                    <img src={image10} alt="Tenth" className="Post-Embed-Image"></img>
+                    <img src={image11} alt="Eleventh" className="Post-Embed-Image"></img>
+                    <img src={image12} alt="Twelveth" className="Post-Embed-Image"></img>
+
+                    <p className="Post-Text">
+                        Some of the pictures of the kit with all the resin pieces on it aren't the best since, as I mentioned above, the parts are basically hanging on by just
+                        hope. It took me a long time to get all the parts even to stay on for these pictures. Despite this though, I'm incredibly happy with the level of detail
+                        the parts added to the kit. I think it makes it look much cooler and so much more interesting than some of the plain, flat parts that came in the base kit.
+                    </p>
+
+                    <img src={image13} alt="Thirteenth" className="Post-Embed-Image"></img>
+                    <img src={image14} alt="Fourteenth" className="Post-Embed-Image"></img>
+                    <img src={image15} alt="Fifteenth" className="Post-Embed-Image"></img>
+                    <img src={image16} alt="Sixteenth" className="Post-Embed-Image"></img>
+                    <img src={image17} alt="Seventeenth" className="Post-Embed-Image"></img>
+
+                    <h2 className="Section">The Electronics</h2>
+                    <p className="Post-Text">
+                        As I mentioned at the start of the post, I wanted to do a ton of LED work in this kit to really make it stand out from all others not just on my shelf, but
+                        all Dynames kits in general.
+                    </p>
+
+                    <h2 className="Section">The Build - Part 3</h2>
+                    <p className="Post-Text">
+                        The last part of the build that needed to be taken care of before painting was adding some additional panel lines on some of the parts that didn't get resin
+                        upgrades and, adding the photoetch parts that came with the resin convserion kit to really add a new level of detail to everything. 
+                    </p>
+                    
+                </div>
+
+                <br />
+                <br />
+                <br />
+            </div>
         </>
     );
 }
